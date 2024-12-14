5c9d2857b08b4fdae10ca60ea7ae190b047134e0
Fallah Andy Prakasa <me@flla.my.id>
2024-12-14 22:31:29 +0700

    fix(jobs): typo column name

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -147,7 +147,7 @@ export const jobs = createTable(
     id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
     name: varchar("name", { length: 255 }).notNull(),
     description: text("description"),
-    isEnabled: boolean("is_active").notNull().default(true),
+    isEnabled: boolean("is_enabled").notNull().default(true),
     cronspec: varchar("cronspec", { length: 255 }).notNull(),
     url: varchar("url", { length: 255 }).notNull(),
     method: varchar("method", { length: 255 }).notNull(),
